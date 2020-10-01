cf2136119ea5062e80dd1ce68615e8cfca1bdbe7
Mrouissi <mariem.rouiissi@gmail.com>
2020-10-01 17:00:21 +0200

    add ghraphView class

---
+++ app/src/main/java/fr/istic/mob/networkLR/GraphView.kt
@@ -0,0 +1,11 @@
+package fr.istic.mob.networkLR
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+
+class GraphView @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+) : View(context, attrs, defStyleAttr) {
+
+}
